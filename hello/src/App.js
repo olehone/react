11f19d1ee50b0f90346fc6c513f219f196525fee
olehvolodko <oleh.volodko.ri.2022@lpnu.ua>
2023-08-04 00:07:16 +0300

    project

---
+++ hello/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, i`m here
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/olehvolodko"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Future updates
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
